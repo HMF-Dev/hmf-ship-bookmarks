d92a4ebf753be119a4fd9a52cc494a362b272b9a
Brendan Moran <b.daniel.moran@gmail.com>
2023-10-24 11:47:58 -0400

    removed file copy, removed transcript output

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,4 @@
-﻿Start-Transcript -Path "$env:PROGRAMDATA\chocolatey\choco-install.log" -Append
-
-try {
+﻿try {
     # Define the Azure Blob Storage URL
     $azureBlobUrl = "https://sennecadeployments.blob.core.windows.net/bookmarks/hmf-ship-clerk.json"
 
@@ -18,34 +16,3 @@ catch {
     Write-Output "Error downloading bookmarks file from Azure Blob Storage"
     Write-Output $_.Exception.Message
 }
-
-
-try {
-    # Define the paths to the Chrome and Edge settings folders in the current user's profile appdata folder, test the paths, and create the folders if they don't exist
-    $chromePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default"
-    $edgePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default"
-    if (!(Test-Path -Path $chromePath)) {
-        Write-Output "Creating $chromePath"
-        New-Item -Path $chromePath -ItemType Directory
-    }
-    if (!(Test-Path -Path $edgePath)) {
-        Write-Output "Creating $edgePath"
-        New-Item -Path $edgePath -ItemType Directory
-    }
-} 
-catch {
-    Write-Output "Error finding or creating Chrome and Edge settings folders"
-    Write-Output $_.Exception.Message
-}
-
-# Now copy the downloaded JSON file to the Chrome and Edge settings folders
-try {
-    Copy-Item -Path "C:\ProgramData\ChocoTemp\Bookmarks" -Destination $chromePath
-    Copy-Item -Path "C:\ProgramData\ChocoTemp\Bookmarks" -Destination $edgePath
-}
-catch {
-    Write-Output "Error copying bookmarks file to Chrome and Edge settings folders"
-    Write-Output $_.Exception.Message
-}
-
-Stop-Transcript
