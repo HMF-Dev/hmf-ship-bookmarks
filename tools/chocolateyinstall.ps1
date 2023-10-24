7f1e34f2fe28c8c1b26c3e9420dae8ed891f882f
Brendan Moran <b.daniel.moran@gmail.com>
2023-10-24 16:43:12 -0400

    remove copy/delete to settings folders

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,28 @@
 ﻿try {
     # Define the Azure Blob Storage URL
+    Write-Output "Getting blob URL"
     $azureBlobUrl = "https://sennecadeployments.blob.core.windows.net/bookmarks/hmf-ship-clerk.json"
 
     #create $env:ProgramData\Temp if it doesn't exist
+    Write-Output "Checking for C:\ProgramData\ChocoTemp"
     if (!(Test-Path -Path "C:\ProgramData\ChocoTemp")) {
         Write-Output "Creating C:\ProgramData\ChocoTemp"
         New-Item -Path "C:\ProgramData\ChocoTemp" -ItemType Directory
     }
 
+    Write-Output "ChocoTemp folder exists, downloading bookmarks file"
+
     # Use Invoke-WebRequest to download the JSON file from Azure Blob Storage to a temp location
     Invoke-WebRequest -Uri $azureBlobUrl -OutFile "C:\ProgramData\ChocoTemp\Bookmarks"
+
+    # check if the bookmarks file exists in the temp folder, and if it does, write success message, if it does not, throw error to catch block
+    if (Test-Path -Path "C:\ProgramData\ChocoTemp\Bookmarks") {
+        Write-Output "Bookmarks file downloaded successfully"
+    }
+    else {
+        Write-Output "Error downloading bookmarks file from Azure Blob Storage"
+        Throw
+    }
 
 } 
 catch {
@@ -17,38 +30,3 @@ catch {
     Write-Output $_.Exception.Message
 }
 
-#prompt the user if they also want to copy the bookmarks to the local user's Chrome and Edge settings folders, format the response as a string, and convert it to uppercase
-#if user doesn't respond in 10 seconds, skip the copy
-$copyBookmarks = Read-Host -Prompt "Copy bookmarks to Chrome and Edge settings folders? (Y/N)" -TimeoutSec 10
-$copyBookmarks = $copyBookmarks.ToString().ToUpper()
-
-if ($copyBookmarks -eq "Y") {
-    try {
-        # Define the paths to the Chrome and Edge settings folders in the current user's profile appdata folder, test the paths, and create the folders if they don't exist  
-        $chromePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default"
-        $edgePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default"
-    
-        if (!(Test-Path -Path $chromePath)) {
-            Write-Output "Creating $chromePath"
-            New-Item -Path $chromePath -ItemType Directory
-        }
-        if (!(Test-Path -Path $edgePath)) {
-            Write-Output "Creating $edgePath"
-            New-Item -Path $edgePath -ItemType Directory
-        }
-    }
-    catch {
-        Write-Output "Error finding or creating Chrome and Edge settings folders"
-        Write-Output $_.Exception.Message
-    }
-    
-    # Now copy the downloaded JSON file to the Chrome and Edge settings folders
-    try {
-        Copy-Item -Path "C:\ProgramData\ChocoTemp\Bookmarks" -Destination $chromePath
-        Copy-Item -Path "C:\ProgramData\ChocoTemp\Bookmarks" -Destination $edgePath
-    }
-    catch {
-        Write-Output "Error copying bookmarks file to Chrome and Edge settings folders"
-        Write-Output $_.Exception.Message
-    }
-}   
