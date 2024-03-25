14d4a0890fb200e71a4ac7cd103980325faf8d0f
Brendan Moran <b.daniel.moran@gmail.com>
2024-03-25 17:37:39 -0400

    Update chocolateyinstall.ps1 to download both JSON and HTML bookmarks files

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
 ﻿try {
     # Define the Azure Blob Storage URL
-    Write-Output "Getting blob URL"
-    $azureBlobUrl = "https://sennecadeployments.blob.core.windows.net/bookmarks/hmf-ship-clerk.json"
+    Write-Output "Getting blob URLs"
+    $jsonBlobUrl = "https://sennecadeployments.blob.core.windows.net/bookmarks/ship_bookmarks"
+    $htmlBlobUrl = "https://sennecadeployments.blob.core.windows.net/bookmarks/ship_bookmarks.html"
 
     #create $env:ProgramData\Temp if it doesn't exist
     Write-Output "Checking for C:\ProgramData\ChocoTemp"
@@ -12,18 +13,26 @@
 
     Write-Output "ChocoTemp folder exists, downloading bookmarks file"
 
-    # Use Invoke-WebRequest to download the JSON file from Azure Blob Storage to a temp location
-    Invoke-WebRequest -Uri $azureBlobUrl -OutFile "C:\ProgramData\ChocoTemp\Bookmarks"
+    # Use Invoke-WebRequest to download files to the temp folder
+    Invoke-WebRequest -Uri $jsonBlobUrl -OutFile "C:\ProgramData\ChocoTemp\Bookmarks"
+    Invoke-WebRequest -Uri $htmlBlobUrl -OutFile "C:\ProgramData\ChocoTemp\bookmarks.html"
 
-    # check if the bookmarks file exists in the temp folder, and if it does, write success message, if it does not, throw error to catch block
+    # check if the bookmarks file exist in the temp folder, and if it does, write success message, if it does not, throw error to catch block
     if (Test-Path -Path "C:\ProgramData\ChocoTemp\Bookmarks") {
-        Write-Output "Bookmarks file downloaded successfully"
+        Write-Output "Bookmarks JSON file downloaded successfully"
     }
     else {
         Write-Output "Error downloading bookmarks file from Azure Blob Storage"
         Throw
     }
 
+    # check if the bookmarks file exist in the temp folder, and if it does, write success message, if it does not, warn user
+    if (Test-Path -Path "C:\ProgramData\ChocoTemp\Bookmarks.html") {
+        Write-Output "Bookmarks HTML file downloaded successfully"
+    }
+    else {
+       Write-Warning "Error downloading bookmarks file from Azure Blob Storage" 
+    }
 } 
 catch {
     Write-Output "Error downloading bookmarks file from Azure Blob Storage"
